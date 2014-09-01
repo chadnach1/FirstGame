deee98d7f8db25eca20b0bf0b22ed44ddec1b15d
chadnach1 <chadn@seas.upenn.edu>
2014-09-01 10:56:38 -0400

    First commit

---
+++ FirstGame/GameOverScene.h
@@ -0,0 +1,15 @@
+//
+//  GameOverScene.h
+//  FirstGame
+//
+//  Created by Chad Nachiappan on 2/4/14.
+//  Copyright (c) 2014 Chad Nachiappan. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface GameOverScene : SKScene
+
+-(id)initWithSize:(CGSize)size won:(BOOL)won;
+
+@end
